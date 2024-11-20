2610c3978501b350a0136c247bf8fed483b9fc84
3lackC4t <C.S.Minty@proton.me>
2024-11-20 17:32:11 -0500

    Initial clone, added some commments for future planning

---
+++ src/__main__.py
@@ -0,0 +1,20 @@
+"""
+Quorus is an attempt at a lightweight and containerizable
+quorum algorith for use in high availability servers.
+"""
+
+# Wait for startup
+
+# Initialize some settings
+
+# Listen for a "Hello" message from something on the config file
+
+# if no message then it promotes itself to a role on the config file 
+
+# If message then it assigns itself a role according to the rules of the config file
+
+def main() -> None:
+    pass
+
+if __name__ == "__main__":
+    main()
