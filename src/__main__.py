f315f1f0fcf2592a10d1a759981f14dfd4d5e713
3lackC4t <C.S.Minty@proton.me>
2024-11-20 17:45:20 -0500

    created a basic UDP listener

--- src/__main__.py
+++ src/__main__.py
@@ -1,7 +1,21 @@
+import os
+import sys
+import socket
+import threading
+import pyaml
+from time import sleep
+
+
 """
 Quorus is an attempt at a lightweight and containerizable
-quorum algorith for use in high availability servers.
+quorum algorithm for use in high availability servers.
 """
+
+GLOBALS = {
+    "PORT": 50515,
+    "ADDRESS": socket.gethostbyname(socket.gethostname)
+}
+
 
 # Wait for startup
 
@@ -12,6 +26,12 @@ quorum algorith for use in high availability servers.
 # if no message then it promotes itself to a role on the config file 
 
 # If message then it assigns itself a role according to the rules of the config file
+
+def initialize() -> None:
+    while True:
+       s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+       s.bind((GLOBALS["ADDRESS"], GLOBALS["PORT"]))
+       s.listen()
 
 def main() -> None:
     pass
