7f283767c407661edbd85b62652ea7b421eaf2a0
3lackC4t <C.S.Minty@proton.me>
2024-11-20 21:51:04 -0500

    Built dispatcher and handler methods

--- src/FireTeam/BaseServer.py
+++ src/FireTeam/BaseServer.py
@@ -1,45 +1,67 @@
 import threading
-import socket
 import json
+import socket
 from datetime import datetime
 
 class Server:
     
     def __init__(self):
         self._name = ""
+        self._status = "CANDIDATE"
         self._address = socket.gethostbyname(socket.gethostname())
         self._default_port = 50515
         self._default_timeout = 15
-        self._exp_clients = []
-        self._clients = []
+        self._exp_clients = [] # The servers that this server believes it should be connected to.
+        self._clients = [] # List of dictionaries containing information about the clients connected to this server.
 
-    def send_msg(self, message_dict) -> None:
-        if message_dict["TYPE"] == "heartbeat":
-            while True:
-                message_dict["TIME_STAMP"] = str(datetime.now())
-                
-                ser_message = json.loads(message_dict)
+    def cluster_init(self, message) -> None:
+        # We do this to initialize our cluster. If the server wakes up and it's client list is empty it will search through the expected clients.
 
-                sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-                for client in self._clients:
-                    sock.sendto(ser_message, (client, self._default_port))
+        announcer = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        encoded_msg = self._Handler("Discover")
 
-        else:
-            message = b"This is a test message"
-            sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    
+        for client in self._clients:            
+            self._Dispatcher(encoded_msg, client) # For now we just goint ouse a raw address
+
+    def send_msg(self, message) -> None:
+        # Sends messages that are built by the dispatcher
+        sender = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+        sender.sendto(message, ())
+
     def recv_msg(self) -> None:
-        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        sock.bind((self._address, self._default_port))
-        sock.settimeout(self._default_timeout)
+        # just sits here and accepts messages and then forwards them to the handler 
+        receiver = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        receiver.bind((self._address, self._default_port))
+        receiver.settimeout(self._default_timeout)
 
         while True:
             try:
-                data, addr = sock.recvfrom(1024)
-                self._clients.append(addr)
-                data = data.decode()
-                print(data)
-            except:
-                print("Something Catastrophic Happened")
+                data, addr = receiver.recvfrom(1024)
+                self._Dispatcher(data)
+                print(f"Received Message from {addr}")
+            except socket.timeout:
+                print(f"No messages received")
 
+    def _Handler(self, msg_type: str):
+        base_msg = {
+            "NAME": self._name,
+            "STATUS": self._status,
+            "ADDRESS": self._address,
+            "TIME_STAMP": str(datetime.now()),
+        }
 
+        if msg_type == "DISCOVER":
+            base_msg["MESSAGE"] = "DISCOVER"
+        elif msg_type == "ANNOUNCE":
+            base_msg["MESSAGE"] = ""
+
+        return json.loads(base_msg)
+
+    def _Dispatcher(self, data, address):
+        data = json.loads(data)
+        message = data["MESSAGE"]
+
+        if message == "DISCOVER":
+            self.send_msg("ANNOUNCE", address)
+        elif message == "ANNOUNCE"
