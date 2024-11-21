07f85fd71e004d641d0c5930d2ec57cb19b67025
3lackC4t <C.S.Minty@proton.me>
2024-11-21 01:54:46 -0500

    Tested communication functionality

--- src/FireTeam/BaseServer.py
+++ src/FireTeam/BaseServer.py
@@ -2,16 +2,18 @@ import threading
 import socket
 import sys
 import os
+import math
 import json
+from time import sleep
 from datetime import datetime
 
 class Node:
     def __init__(self):
         self.default_port = 50515
-        self.address = socket.gethostbyname(socket.gethostname())
+        self.address = "127.0.0.1" 
         self.default_buffer = 4096
 
-        self.id = ""
+        self.id = f"{self.address}:{self.default_port}"
         self.current_term = 0
         self.voted_for = None
         self.current_role = "FOLLOWER"
@@ -22,15 +24,15 @@ class Node:
         self.commit_length = []
         self.sent_length = []
 
-        self.nodes_expected = {}
-        self.nodes = {}
+        self.nodes_expected = []
+        self.nodes = []
 
         self._handler = Handler()
         self._dispatcher = Dispatcher()
 
     def listen(self) -> None:
-        with open(socket.socket(socket.AF_INET, socket.SOCK_DGRAM)) as receiver:
-            receiver.bind((self.address, self.default_port))
+        with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as receiver:
+            receiver.bind(("0.0.0.0", self.default_port))
             print(f"Node:{self.id}::listening on Port:{self.default_port}::Status:{self.current_role}")
 
             while True:
@@ -38,17 +40,15 @@ class Node:
                     data, sender = receiver.recvfrom(self.default_buffer)
                     print(f"RCVD::Sender:{sender}::data:{data}")
                     self._handler.de_serialize(data, sender)
-
+                    print(self._handler.data)
                 except socket.timeout:
-                    pass
+                    print("timieout occured while listening")
                 except OSError:
-                    pass
-                finally:
-                    pass
+                    print("something attempted to connect but failed")
 
     def send_msg(self, address, port, data) -> None:
-        with open(socket.socket(socket.AF_INET, socket.SOCK_DGRAM)) as sender:
-            sender.sendto(data, (address, port))
+        with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sender:
+            sender.sendto(self._dispatcher.format(data), (address, port))
 
     def set_role(self, role):
         self.current_role = role
@@ -71,6 +71,9 @@ class Node:
             last_term = self.log[len(self.log - 1)]["TERM"]
         
         msg = self._dispatcher.format({
+            "ID": self.id,
+            "ADDRESS": self.address,
+            "PORT": self.default_port,
             "MSG_TYPE": "ELECTION_TIMEOUT",
             "NODE_ID": self.id, 
             "CURRENT_TERM" : self.current_term,
@@ -89,29 +92,69 @@ class Node:
 
         last_term = 0
         if len(self.log) > 0:
-            last_term = self.log[len(self.log -1)]["TERM"]
+            last_term = self.log[len(self.log) -1]["TERM"]
 
         logOk = (last_term <= log_term) or (log_term == last_term and len(self.log) < log_len )
 
-        if self.current_term >= curr_term and logOk and self.voted_for is not None:
+        if self.current_term >= curr_term and logOk and self.voted_for is None:
             self.voted_for = id
 
             msg = self._dispatcher({
-                "MSG_TYPE" : "VOTE_RESPONSE",
                 "ID": self.id,
+                "ADDRESS": self.address,
+                "PORT": self.default_port,               
+                "MSG_TYPE" : "VOTE_RESPONSE",
                 "CURRENT_TERM": self.current_term,
-                "RESPONSE": True
+                "RESPONSE": True,
             })
 
-            self.send_msg()         
+            return msg
+        else:
+            msg = self._dispatcher({
+                "ID": self.id,
+                "ADDRESS": self.address,
+                "PORT": self.default_port,              
+                "MSG_TYPE" : "VOTE_RESPONSE",
+                "CURRENT_TERM": self.current_term,
+                "RESPONSE": False, 
+            })
 
-               
+            return msg       
+
+    def vote_response(self, voterId, term, granted):
+        if self.current_role == "CANDIDATE" and term == self.current_term and granted:
+            self.votes_recieved.append(voterId)
+        
+        if len(self.votes_recieved) >= math.ceil((len(self.nodes) + 1) / 2):
+            self.current_role = "LEADER"
+            self.current_leader = self.id
+            for client in self.clients:
+                pass
+
+    def election_timer_reset(self):
+        pass
+
+    def send_heartbeat(self):
+        while self.current_role == "LEADER":
+            msg = self._dispatcher({
+                "ID": self.id,
+                "ADDRESS": self.address,
+                "PORT": self.default_port,              
+                "MSG_TYPE" : "VOTE_RESPONSE",
+                "CURRENT_TERM": self.current_term,
+            })
+
+            for client in self.nodes:
+                self.send_msg(client["ADDRESS"], client["PORT"], msg)
+            
+            sleep(1)
+        sleep(1)
 
 # The Handler is responsible for formatting incoming messages
 # Raw stream data is sent to the handler, decoded and then returned
 class Handler:
     def __init__(self):
-        self.data = {{}, ""}
+        self.data = [{}, ""]
 
     def de_serialize(self, data, sender):
         self.data[0] = json.loads(data)
@@ -125,5 +168,5 @@ class Dispatcher:
     def __init__(self):
         pass
 
-    def format(self, params):
-        pass
+    def format(self, msg):
+        return json.dumps(msg).encode("utf-8")
--- src/__main__.py
+++ src/__main__.py
@@ -1,8 +1,22 @@
-from FireTeam.BaseServer import Server
+from FireTeam.BaseServer import Node
+
+import threading
 
 
 def main() -> None:
-    pass 
+    server1 = Node()
+    server1.default_port = 50515
+    server2 = Node()
+    server2.default_port = 50516
+
+    threading.Thread(target=server1.listen, daemon=False).start()
+    threading.Thread(target=server2.send_msg(server1.address, server1.default_port, {
+        "ID": server2.id,
+        "ADDRESS": server2.address,
+        "PORT": server2.default_port,
+        "ROLE": server2.current_role,
+        "MSG_TYPE": "TEST"
+    }), daemon=False).start()
 
 if __name__ == "__main__":
     main()
